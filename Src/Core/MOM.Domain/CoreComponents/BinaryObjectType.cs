d3246f1a431f0e633646c060ee5330ff9daa2562
王振耀 <wsbp2019@163.com>
2025-03-24 19:24:43 +0800

    B2MML-CoreComponents

---
+++ Src/Core/MOM.Domain/CoreComponents/BinaryObjectType.cs
@@ -0,0 +1,34 @@
+﻿using MOM.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOM.Domain.CoreComponents
+{
+    /// <summary>
+    /// 用于定义表示图形、图片、声音、视频或其他形式的数据的数据类型，这些数据可以表示为二进制八位字节的有限长度序列
+    /// </summary>
+    internal class BinaryObjectType : BaseEntity
+    {
+        private BinaryObjectType() : base()
+        {
+
+        }
+        public BinaryObjectType(string name, byte[] content) : this()
+        {
+            Id = name;
+            Content = content;
+        }
+        [NotMapped]
+        public string Name { get{ return Id; } }
+        public byte[] Content { get; set; }
+
+        internal void UpdateContent(byte[] content)
+        {
+            Content = content ?? [];
+        }
+    }
+}
