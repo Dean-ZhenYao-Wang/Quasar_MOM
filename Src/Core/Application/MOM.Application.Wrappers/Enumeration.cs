13050331e1dd69358333936f97d198ad6e37835c
王振耀 <wsbp2019@163.com>
2025-04-04 09:09:53 +0800

    1

---
+++ Src/Core/Application/MOM.Application.Wrappers/Enumeration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOM.Application.Wrappers
+{
+    public abstract class Enumeration<TEnum> : IEquatable<Enumeration<TEnum>>
+    where TEnum : Enumeration<TEnum>
+    {
+        public int Id { get; }
+        public string Name { get; }
+
+        protected Enumeration(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public static IReadOnlyCollection<TEnum> All =>
+            typeof(TEnum)
+                .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+                .Select(f => f.GetValue(null))
+                .Cast<TEnum>()
+                .ToList();
+
+        public static TEnum? FromId(int id) => All.FirstOrDefault(x => x.Id == id);
+        public static TEnum? FromName(string name) => All.FirstOrDefault(x => x.Name == name);
+
+        public bool Equals(Enumeration<TEnum>? other) =>
+            other is not null && GetType() == other.GetType() && Id == other.Id;
+
+        public override bool Equals(object? obj) => obj is Enumeration<TEnum> other && Equals(other);
+
+        public override int GetHashCode() => HashCode.Combine(Id, GetType());
+
+        public override string ToString() => Name;
+
+        public static bool operator ==(Enumeration<TEnum>? left, Enumeration<TEnum>? right) =>
+            Equals(left, right);
+
+        public static bool operator !=(Enumeration<TEnum>? left, Enumeration<TEnum>? right) =>
+            !Equals(left, right);
+    }
+}
--- Src/Core/MOM.Domain/Common/BaseEntity.cs
+++ Src/Core/MOM.Domain/Common/BaseEntity.cs
@@ -12,7 +12,6 @@ namespace MOM.Domain.Common
         /// 名称
         /// 对象ID仅仅用于在相关的交换信息集中确认该对象。对象ID属性不是全局的对象ID，也不是数据库索引属性。
         /// </summary>
-        [JsonPropertyName("Id")]
         public string Id { get; set; }
         public Guid CreatedBy { get; set; }
         public DateTime Created { get; set; }
