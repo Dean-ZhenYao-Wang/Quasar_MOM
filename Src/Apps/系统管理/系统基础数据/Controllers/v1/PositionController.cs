cd0f244075608a2f5a9190e7870c2f96a115c4d1
王振耀 <wsbp2019@163.com>
2025-05-17 10:19:47 +0800

    1

---
+++ Src/Apps/系统管理/系统基础数据/Controllers/v1/PositionController.cs
@@ -0,0 +1,14 @@
+﻿using Asp.Versioning;
+using MOM.Application.Infrastructure;
+
+namespace 系统基础数据.Controllers.v1
+{
+    /// <summary>
+    /// 职位管理
+    /// </summary>
+    [ApiVersion("1")]
+    public sealed class PositionController : BaseApiController
+    {
+
+    }
+}
