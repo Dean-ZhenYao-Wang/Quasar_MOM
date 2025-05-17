f6f93593e8b358978b30a07bf412fdea6ec76ffd
王振耀 <wsbp2019@163.com>
2025-05-17 17:37:23 +0800

    1

--- Src/Core/Application/Features/MOM.Application.Features.HierarchyScope/Queries/GetOrgTable/GetOrgTableQueryHandler.cs
+++ Src/Core/Application/Features/MOM.Application.Features.HierarchyScope/Queries/GetOrgTable/GetOrgTableQueryHandler.cs
@@ -11,11 +11,11 @@ using System.Threading.Tasks;
 
 namespace MOM.Application.Features.HierarchyScope.Queries.GetOrgTable
 {
-    public class GetOrgTableQueryHandler(IHierarchyScopeRepository hierarchyScopeRepository) : IRequestHandler<GetOrgTableQuery, PagedResponse<OrgResponse>>
+    public class GetOrgTableQueryHandler(IHierarchyScopeContainsRelationshipRepository hierarchyScopeContainsRelationshipRepository) : IRequestHandler<GetOrgTableQuery, PagedResponse<OrgResponse>>
     {
         public async Task<PagedResponse<OrgResponse>> Handle(GetOrgTableQuery request, CancellationToken cancellationToken)
         {
-            return await hierarchyScopeRepository.GetOrgListAsync(request.SourceDtId, request.Id, request.Name, request.Page, request.PageSize);
+            return await hierarchyScopeContainsRelationshipRepository.GetOrgListAsync(request.SourceDtId, request.Id, request.Name, request.Page, request.PageSize);
         }
     }
 }
--- Src/Core/Application/Features/MOM.ApplicationFeatures.Permission/Commands/AddMenu/AddMenuCommandHandler.cs
+++ Src/Core/Application/Features/MOM.ApplicationFeatures.Permission/Commands/AddMenu/AddMenuCommandHandler.cs
@@ -15,7 +15,7 @@ namespace MOM.Application.Features.Permission.Commands.AddMenu
     {
         public async Task<BaseResult> Handle(AddMenuCommand request, CancellationToken cancellationToken)
         {
-            if (buttonRepository.Where(m => m.Id.Equals(request.Id)).Any() || menuRepository.Where(m => m.DtId != entity.DtId && m.Id.Equals(request.Id)).Any())
+            if (buttonRepository.Where(m => m.Id.Equals(request.Id)).Any() || menuRepository.Where(m => m.DtId != request.DtId && m.Id.Equals(request.Id)).Any())
                 throw new ApplicationException("菜单编号必须唯一");
             var addMenu = request.ToMenu();
             await menuRepository.AddAsync(addMenu);
